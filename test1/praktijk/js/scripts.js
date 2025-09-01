f0ac7a9408ab82ac2a8fd1cf79a7b4b6d462184a
alaabdww <91311739+alaabdww@users.noreply.github.com>
2025-09-01 22:33:58 +0200

    *

---
+++ test1/praktijk/js/scripts.js
@@ -0,0 +1,45 @@
+const quotes = [
+   'Leven ons vraag jonge goa ouder. Schatkist ad aardschok bepaalden producten ik gomboomen.',
+   'Planters schatten overgaat troepjes en baksteen behouden in',
+   'Om eerder groene gronds de soegei bamboe in dreigt af',
+   'Schaal zoodat bakjes te slotte en'
+];
+
+const pictures = [
+   'img/profile1a.jpg',
+   'img/profile1b.jpg',
+   'img/profile1c.jpg'
+];
+
+// DOM
+// ...
+
+// 1. header background color
+// ...
+
+// 2. random picture
+// ...
+
+// 3. next quote
+// ...
+
+// 4. fav link toggle
+// ...
+
+// 5. flip
+// ...
+
+// 6. keyboard flip		
+// ...
+
+// 7. show contact form
+// ...
+
+// 8. hide contact form
+// ...
+
+// 9. show characters typed
+// ...
+
+// 10. formchecking
+// ...
